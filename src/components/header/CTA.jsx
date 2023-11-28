2d2c2917a035bf5b0778eebaf30f892370f0837c
TarifSadman <sadmantaha17@gmail.com>
2023-11-28 14:33:40 +0600

    firestore update

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import CV from '../../assets/Tarif Sadman.pdf';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const CTA = () => {
+  const [resume, setResume] = useState(null);
+
+  useEffect(() => {
+    axios.get('https://cooperative-calf-slippers.cyclic.app/api/resume') // Update with the correct URL
+      .then((res) => {
+        setResume(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="cta">
-      <a href={CV} download className="btn">
+      <a href={resume?.res} target='_blank' rel='noreferrer' download className="btn">
         Download CV
       </a>
       <a href="#contact" className="btn btn-primary">
